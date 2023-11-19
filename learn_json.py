0b070cf0333ab87c707b68c0692e976baa0680a4
Zyno16 <135590226+Zyno16@users.noreply.github.com>
2023-11-19 14:09:51 +0000

    Add files via upload

---
+++ learn_json.py
@@ -0,0 +1,18 @@
+ 
+
+# object
+names ={"zino" :"2" , "soumeya" :"sister" , "ines ":"daughter"}
+#value or arary
+
+
+print (============)
+data = ["ahmed","zino" ,"yusra"]
+#example for string boolean number
+string >> "ahmed" >> {"fname" : "amr"}
+.....................................
+number >> 3500 >> {"salary" : 4500 }
+.................................
+boolean >> true , false >> { "isactive" : true} >> {"isactive" : false }
+
+array >> [2,21,54,87] >> ["a","b","c"]
+
