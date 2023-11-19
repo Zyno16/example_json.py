c042ee3816e98a7d6b12e77293116684654e5b99
Zyno16 <135590226+Zyno16@users.noreply.github.com>
2023-11-19 14:23:38 +0000

    Add files via upload

---
+++ json_ example.2.py
@@ -0,0 +1,5 @@
+>> "ahmedr\b" >> this is to delete the letter r
+>> "ahmed\n ">> this is to go to new line
+>> "ahmed \r" >> this to delete the word 
+>> "hassouna\tAcademy" >> this t like Tab
+>> "\"ahmed\"" >> this is to write the name in double cute 
